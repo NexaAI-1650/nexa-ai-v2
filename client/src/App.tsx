d13d1c49e3a7ce7d59a006d9de6a47898c1e63b9
NexaAI-1650 <kuri.gawrgura717@gmail.com>
2025-11-25 15:12:37 +0900

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,13 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
-import ChatPage from "@/pages/chat";
 import AdminDashboard from "@/pages/admin-dashboard";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={ChatPage} />
+      <Route path="/" component={AdminDashboard} />
       <Route path="/admin" component={AdminDashboard} />
       <Route component={NotFound} />
     </Switch>
